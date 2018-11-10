a1269ba8ecd63fe1af7609b17d1d6f6830b488ff
Robin <robinleesuns13@gmail.com>
2018-11-09 16:04:40 -0800

    routes adjusting

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router();
-const {addPage, wikiPage} = require("../views");
+const {addPage, wikiPage, main} = require("../views");
 const {Page} = require('../models');
 
 
 router.get('/', async (req, res, next)=>{
-  const found = await Page.findAll()
+  try{
+    const all = await Page.findAll()
+    res.send(main(all))
+  } catch(error){next(error)}
 
-  res.send(wikiPage(found));
 })
 
 router.post('/', async (req, res, next) => {
@@ -24,7 +26,7 @@ router.post('/', async (req, res, next) => {
   // note: `.save` returns a promise.
   try {
     await page.save();
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) { next(error) }
 });
 
@@ -39,6 +41,7 @@ router.get('/:slug', async (req, res, next)=>{
         slug: req.params.slug
       }
     })
+    res.send(wikiPage(page));
     res.json(page)
   } catch(error) {next(error)}
 })
